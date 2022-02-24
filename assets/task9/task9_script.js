b76436f4e3bfa233b4bff92f272bc2a170a6ac64
Igor-Polonskiy <53859088+Igor-Polonskiy@users.noreply.github.com>
2022-02-24 10:28:24 +0300

    task9

---
+++ assets/task9/task9_script.js
@@ -0,0 +1,33 @@
+(() => {
+    const reloadTaskBtn = document.querySelector('.task9_reloadTask')
+    const checkingTaskBtn = document.querySelector('.task9_checkingTask')
+    const checkTask = document.querySelector('.task9_checkTask')
+    const chek_answerTxt = document.querySelector('.task9_chek_answer')
+    const inputBlue = document.querySelector('#task9_blue')
+
+    const winVarTask3 = '10'
+
+    let answer1 = 0
+ 
+
+    inputBlue.addEventListener('change', (e) => {
+        answer1 = e.target.value
+    })
+   
+    reloadTaskBtn.addEventListener('click', () => {
+        inputBlue.value = ''
+        chek_answerTxt.innerHTML = ''
+        checkTask.style.background = ''
+    })
+
+    checkingTaskBtn.addEventListener('click', () => {
+        if (answer1 ===  winVarTask3) {
+            chek_answerTxt.innerHTML = '<span>&#128077;</span> Молодец!'
+            checkTask.style.background = 'lightgreen'
+        } else {
+            chek_answerTxt.innerHTML = '<span>&#10060;</span> Попробуй еще!'
+            checkTask.style.background = 'lightpink'
+        }
+    })
+
+})()
