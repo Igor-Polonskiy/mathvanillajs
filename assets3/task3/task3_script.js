2749c2dbea0f5329d7f5a03cddbe56e2e1added9
Polonskiy Igor <barlogfire@ya.ru>
2022-03-02 15:09:45 +0300

    less3 task3

---
+++ assets3/task3/task3_script.js
@@ -0,0 +1,66 @@
+(() => {
+    const pencils = document.querySelector('.task3_pencils')
+    const pencilsImg = document.querySelectorAll('.task3_pencilImg')
+    const circleWrapper = document.querySelector('.task3_circleWrapper')
+    const baloons = document.querySelectorAll('.task3_baloon')
+    const reloadTaskBtn = document.querySelector('.task3_reloadTask')
+    const checkingTaskBtn = document.querySelector('.task3_checkingTask')
+    const checkTask = document.querySelector('.task3_checkTask')
+    const chek_answerTxt = document.querySelector('.task3_chek_answer')
+
+    let color = 'white'
+
+    pencils.addEventListener('click', (e) => {
+        if (e.target.classList.contains('task3_pencilImg')) {
+            pencilsImg.forEach(item => {
+                item.classList.remove('task3_pencilSelect')
+            })
+            e.target.classList.add('task3_pencilSelect')
+            color = e.target.getAttribute('data-color')
+        }
+    })
+
+    circleWrapper.addEventListener('click', (e) => {
+        if (e.target.closest('.task3_baloon')) {
+            e.target.closest('.task3_baloon').setAttribute('fill', color)
+        }
+    })
+
+    reloadTaskBtn.addEventListener('click', () => {
+        pencilsImg.forEach(item => {
+            item.classList.remove('task3_pencilSelect')
+        })
+        color = 'white'
+        baloons.forEach(item => {
+            item.setAttribute('fill', color)
+        })
+        chek_answerTxt.innerHTML = ''
+        checkTask.style.background = ''
+    })
+
+    checkingTaskBtn.addEventListener('click', () => {
+        let winVar = 0
+
+        baloons.forEach(item => {
+            if (item.classList.contains('yellow')) {
+                if (item.getAttribute('fill') === 'yellow') {
+                    winVar++
+                }
+            }
+            if (item.classList.contains('green')) {
+                if (item.getAttribute('fill') === 'green') {
+                    winVar++
+                }
+            }
+        })
+
+        if (winVar === 4) {
+            chek_answerTxt.innerHTML = '<span>&#128077;</span> Молодец!'
+            checkTask.style.background = 'lightgreen'
+        } else {
+            chek_answerTxt.innerHTML = '<span>&#10060;</span> Попробуй еще!'
+            checkTask.style.background = 'lightpink'
+        }
+    })
+
+})()
