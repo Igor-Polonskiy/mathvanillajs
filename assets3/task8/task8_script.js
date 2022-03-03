a1415eea34b9093eceacf5467eb520f12f241816
Polonskiy Igor <barlogfire@ya.ru>
2022-03-03 09:56:06 +0300

    less3 task7-8

--- assets3/task8/task8_script.js
+++ assets3/task8/task8_script.js
@@ -1,34 +1,34 @@
 (() => {
-    const pencils = document.querySelector('.task7_pencils')
-    const pencilsImg = document.querySelectorAll('.task7_pencilImg')
-    const circleWrapper = document.querySelector('.task7_circleWrapper')
-    const baloons = document.querySelectorAll('.task7_baloon')
-    const reloadTaskBtn = document.querySelector('.task7_reloadTask')
-    const checkingTaskBtn = document.querySelector('.task7_checkingTask')
-    const checkTask = document.querySelector('.task7_checkTask')
-    const chek_answerTxt = document.querySelector('.task7_chek_answer')
+    const pencils = document.querySelector('.task8_pencils')
+    const pencilsImg = document.querySelectorAll('.task8_pencilImg')
+    const circleWrapper = document.querySelector('.task8_circleWrapper')
+    const baloons = document.querySelectorAll('.task8_baloon')
+    const reloadTaskBtn = document.querySelector('.task8_reloadTask')
+    const checkingTaskBtn = document.querySelector('.task8_checkingTask')
+    const checkTask = document.querySelector('.task8_checkTask')
+    const chek_answerTxt = document.querySelector('.task8_chek_answer')
 
     let color = 'white'
 
     pencils.addEventListener('click', (e) => {
-        if (e.target.classList.contains('task7_pencilImg')) {
+        if (e.target.classList.contains('task8_pencilImg')) {
             pencilsImg.forEach(item => {
-                item.classList.remove('task7_pencilSelect')
+                item.classList.remove('task8_pencilSelect')
             })
-            e.target.classList.add('task7_pencilSelect')
+            e.target.classList.add('task8_pencilSelect')
             color = e.target.getAttribute('data-color')
         }
     })
 
     circleWrapper.addEventListener('click', (e) => {
-        if (e.target.closest('.task7_baloon')) {
-            e.target.closest('.task7_baloon').setAttribute('fill', color)
+        if (e.target.closest('.task8_baloon')) {
+            e.target.closest('.task8_baloon').setAttribute('fill', color)
         }
     })
 
     reloadTaskBtn.addEventListener('click', () => {
         pencilsImg.forEach(item => {
-            item.classList.remove('task7_pencilSelect')
+            item.classList.remove('task8_pencilSelect')
         })
         color = 'white'
         baloons.forEach(item => {
@@ -42,24 +42,20 @@
         let winVar = 0
 
         baloons.forEach(item => {
-            if (item.classList.contains('task7_yellow')) {
+            if (item.classList.contains('task8_yellow')) {
                 if (item.getAttribute('fill') === 'yellow') {
                     winVar++
                 }
             }
-            if (item.classList.contains('task7_green')) {
-                if (item.getAttribute('fill') === 'green') {
-                    winVar++
-                }
-            }
-            if (item.classList.contains('task7_brown')) {
+
+            if (item.classList.contains('task8_brown')) {
                 if (item.getAttribute('fill') === 'brown') {
                     winVar++
                 }
             }
         })
 
-        if (winVar === 3) {
+        if (winVar === 2) {
             chek_answerTxt.innerHTML = '<span>&#128077;</span> Молодец!'
             checkTask.style.background = 'lightgreen'
         } else {
